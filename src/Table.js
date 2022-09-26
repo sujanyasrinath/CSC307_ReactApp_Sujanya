43dea90b77fd17eb374a918bb48929791d7867f1
Sujanya Srinath <sujanya@Sujanyas-MacBook-Air.local>
2022-09-25 23:06:36 -0700

    Stage 4 checkpoint

--- src/Table.js
+++ src/Table.js
@@ -3,39 +3,43 @@ import React from 'react'
 
 
 function TableHeader()  {
+  return (
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Remove</th>
+      </tr>
+    </thead>
+  );
+}
+
+function TableBody(props) {
+  const rows = props.characterData.map((row, index) => {
     return (
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Job</th>
-        </tr>
-      </thead>
+      <tr key={index}>
+      <td>{row.name}</td>
+      <td>{row.job}</td>
+      <td>
+        <button onClick={() => props.removeCharacter(index)}>Delete</button>
+      </td>
+      </tr>
     );
-  }
-  function TableBody(props) {
-    const rows = props.characterData.map((row, index) => {
-      return (
-        <tr key={index}>
-          <td>{row.name}</td>         
-          <td>{row.job}</td>
-        </tr>
-      );
-     }
-    );
-    return (
-        <tbody>
-          {rows}
-         </tbody>
-     );
-  }
+  });
+  return (
+    <tbody>
+      {rows}
+    </tbody>
+  );
+} 
   
-  
-  function Table(props) {
-      return (
-        <table>
-          <TableHeader />
-          <TableBody characterData={props.characterData} />
-        </table>
-      );
-  }
-  export default Table;
+function Table (props) {
+  return (
+    <table>
+      <TableHeader />
+      <TableBody characterData={props.characterData} removeCharacter={props.removeCharacter} />
+    </table>
+  );
+}
+
+export default Table;
